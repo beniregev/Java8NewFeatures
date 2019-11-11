b675bfb0fb1d4951a3c0b3b38522b88bbb538ff6
Binyamin Regev <binyamin.regev@nice.com>
2019-11-11 16:05:31 +0200

    Finishing the example of Method Reference static and non-static method

--- src/main/java/com/beniregev/e_methodreference/Java8MethodReferenceExample.java
+++ src/main/java/com/beniregev/e_methodreference/Java8MethodReferenceExample.java
@@ -16,12 +16,101 @@ public class Java8MethodReferenceExample {
      * <h4>We Are done!</h4>>
      */
     public void java8MethodReferencePrintln() {
+        System.out.println("java8MethodReferencePrintln():");
         names.forEach(System.out::println);
+        System.out.println("--------------------------------------------------------------------------------------------------");
+    }
+
+    public void beforeJava8MethodReferenceWithMyOwnImplementation() {
+        System.out.println("beforeJava8CallingMethodReferenceWithMyOwnImplementation(): ");
+
+        String str = "Nice Company";
+        MyPrinter mp = new MyPrinter();
+        mp.print(str);
+        System.out.println("--------------------------------------------------------------------------------------------------");
+    }
+
+    public void java8NoMethodReferenceWithOwnImplementation() {
+        System.out.println("java8NoMethodReferenceWithOwnImplementation(): ");
+
+        MyPrinter mp = new MyPrinter();
+        mp.print("Nice", new Parser() {
+            @Override
+            public String parse(String str) {
+                return StringParser.staticConvert(str);
+            }
+        });
+        mp.print("Nice Company", new Parser() {
+            @Override
+            public String parse(String str) {
+                return StringParser.staticConvert(str);
+            }
+        });
+        System.out.println("--------------------------------------------------------------------------------------------------");
+    }
+
+    public void java8MethodReferenceWithOwnImplementation() {
+        System.out.println("java8MethodReferenceWithOwnImplementation(): ");
+
+        MyPrinter mp = new MyPrinter();
+        //  Lambda
+        System.out.println("\tCalling a static method using Lambda");
+        mp.print("Nice", (str -> StringParser.staticConvert(str)));
+        mp.print("Nice Company", (str -> StringParser.staticConvert(str)));
+
+        //  Method Reference - calling a static method
+        System.out.println("\tMethod Reference - calling a static method");
+        mp.print("Nice", (StringParser::staticConvert));
+        mp.print("Nice Company", (StringParser::staticConvert));
+
+        //  Method Reference - calling a non-static method
+        System.out.println("\tMethod Reference - calling a non-static method");
+        StringParser sp = new StringParser();
+        mp.print("Nice", (sp::nonStaticConvert));
+        mp.print("Nice Company", (sp::nonStaticConvert));
+
+
+        System.out.println("--------------------------------------------------------------------------------------------------");
     }
 
     public static void main(String[] args) {
         Java8MethodReferenceExample example = new Java8MethodReferenceExample();
 
         example.java8MethodReferencePrintln();
+        example.beforeJava8MethodReferenceWithMyOwnImplementation();
+        example.java8NoMethodReferenceWithOwnImplementation();
+        example.java8MethodReferenceWithOwnImplementation();
+    }
+}
+
+class MyPrinter {
+    public void print(String str) {
+        System.out.println(str);
+    }
+    public void print(String str, Parser p) {
+        str = p.parse(str);
+        System.out.println(str);
+    }
+}
+
+interface Parser {
+    String parse(String str);
+}
+
+class StringParser {
+    public static String staticConvert(String s) {
+        if (s.length() <= 4)
+            s = s.toUpperCase();
+        else
+            s = s.toLowerCase();
+        return s;
+    }
+
+    public String nonStaticConvert(String s) {
+        if (s.length() <= 4)
+            s = s.toUpperCase();
+        else
+            s = s.toLowerCase();
+        return s;
     }
 }
