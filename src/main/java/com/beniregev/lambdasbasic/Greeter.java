7977c12cd66e8178352d3c2b5d4494158720ab70
binyamin.regev <beniregev@gmail.com>
2021-01-08 18:19:32 +0200

    Minor changes

--- src/main/java/com/beniregev/lambdasbasic/Greeter.java
+++ src/main/java/com/beniregev/lambdasbasic/Greeter.java
@@ -4,11 +4,11 @@ package com.beniregev.lambdasbasic;
  * Before Java8 when we wanted to write a simple program to print "Hello World"
  * then we needed to write something like the following class {@code Greeter}.
  * <br />
- * @author Binyamin Regev email: beniregev@gmail.com
+ * @author Binyamin Regev
  */
 public class Greeter {
     public void greet() {
-        System.out.println("Hello World!");
+        System.out.println("Greeter.greet() -- Hello World!");
     }
 
     public void greetUsingInterface(Greeting greeting) {
