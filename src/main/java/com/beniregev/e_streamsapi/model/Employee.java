0e89cb63374150af530ed20ef684ec38d03e9351
binyamin.regev <beniregev@gmail.com>
2021-01-18 08:33:25 +0200

    Removing Id annotation from id property of Employee class

--- src/main/java/com/beniregev/e_streamsapi/model/Employee.java
+++ src/main/java/com/beniregev/e_streamsapi/model/Employee.java
@@ -2,24 +2,30 @@ package com.beniregev.e_streamsapi.model;
 
 import lombok.*;
 
-import javax.persistence.Id;
-
 /**
+ * <p>  Model {@code Employee} class used in demonstration and examples
+ *      of Java 8 Stream API. </p>
  * <p>
- * Model {@code Employee} class used in demonstration
- * and examples of Java 8 Stream API.
+ *     <ul>Using {@code Data} annotation includes the following annotaions inside:
+ *     <li>Getter</li>
+ *     <li>Setter</li>
+ *     <li>EqualsAndHashCode</li>
+ *     <li>ToString</li>
+ *     <li>RequiredArgsConstructor</li>
+ *     <li>lombok.Value</li>
+ *     </ul>
  * </p>
  * @author Binyamin Regev e-mail: beniregev@gmail.com
  * @since 1.8
  */
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+//@ToString
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
 public class Employee {
-    @Id
     private Integer id;
     private String firstName;
     private String lastName;
