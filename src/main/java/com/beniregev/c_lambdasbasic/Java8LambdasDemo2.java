75d70523bfbbca80e5a9eaeb11ce06b5cb880944
binyamin.regev <beniregev@gmail.com>
2022-05-07 12:28:23 +0300

    Making property names final

--- src/main/java/com/beniregev/c_lambdasbasic/Java8LambdasDemo2.java
+++ src/main/java/com/beniregev/c_lambdasbasic/Java8LambdasDemo2.java
@@ -16,7 +16,7 @@ import java.util.function.Consumer;
  * @since 1.8
  */
 public class Java8LambdasDemo2 {
-    private List<String> names = Arrays.asList("Avraham", "Sarah", "Itshak", "Rivka");
+    private final List<String> names = Arrays.asList("Avraham", "Sarah", "Itshak", "Rivka");
     public void simpleIterationOnListWithoutLambda() {
         System.out.println("simpleIterationOnListWithoutLambda(): ");
         Consumer<String> con = new Consumer<String>() {
@@ -50,7 +50,7 @@ public class Java8LambdasDemo2 {
      *          Consumer<String> con = String s -> System.out.println(s);
      *          names.forEach(con);
      *      </code>
-     * 6. Using Lambdas we don't need to state the datatype of the argumentm so, "String" goes as well:
+     * 6. Using Lambdas we don't need to state the datatype of the arguments so, "String" goes as well:
      *      <code>
      *          Consumer<String> con = s -> System.out.println(s);
      *          names.forEach(con);
@@ -91,6 +91,41 @@ public class Java8LambdasDemo2 {
             System.out.println("\tSubstrings: \"" + stringOfSubstrings + "\"");
         });
         System.out.println("--------------------------------------------------------------------------------------------------");
+        phrases.forEach(name -> {
+            List<String> listOfWords = Arrays.asList(name.split("^[-_]+$"));
+            StringBuilder sb = new StringBuilder(listOfWords.get(0));
+            for(int i=1; i < listOfWords.size(); i++)
+                sb.append(listOfWords.get(i).substring(0, 1).toUpperCase() + listOfWords.get(i).substring(1));
+            System.out.println("\tSubstrings: \"" + sb.toString() + "\"");
+        });
+        System.out.println("--------------------------------------------------------------------------------------------------");
+    }
+
+    /**
+     * Convert a given string containing words separated by underscore (_), hyphen (-), pipe (|), or space to Camel Case.
+     * If the first word start with a lower case letter then it will be lower-case camel-case.
+     * If the first word start with an UPPER case letter then it will be UPPER-case camel-case.
+     */
+    public void java8ToCamelCase(){
+        System.out.println("java8ToCamelCase(): ");
+        List<String> phrases = Arrays.asList("do or do not there is no try",
+                "I-drink-and-i-know-things",
+                "in_the_game_of_Thrones_you_win_or_you_die",
+                "How|did|you|survived|knife|to|your|heart?|I|did|not",
+                "Hold the door",
+                "Live-Long_and|prosper",
+                "I have-always_been|and-will-always|be your_friend",
+                "He|is|the_one|who|was.you-are-the_one_who_is.and.he.is.the one|who_will_be"
+        );
+        phrases.forEach(phrase -> {
+            String[] words = phrase.split("[-\\.|_\\s]");
+            String camelCase = Arrays.stream(words, 1, words.length)
+                    .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                    .reduce(words[0], String::concat);
+            System.out.println("\tcamelCase: \"" + camelCase + "\"");
+        });
+
+        System.out.println("--------------------------------------------------------------------------------------------------");
     }
 
     public static void main(String[] args) {
@@ -99,5 +134,6 @@ public class Java8LambdasDemo2 {
         demo.simpleIterationOnListWithoutLambda();
         demo.java8SimpleIterationOnList();
         demo.java8LambdasExpressions();
+        demo.java8ToCamelCase();
     }
 }
